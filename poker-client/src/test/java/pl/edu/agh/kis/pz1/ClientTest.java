c0c820d0382242f7fa2825f6a2c3f25c01c21ea2
Paulina Gacek <paulina.gacek.pl@gmail.com>
2021-11-10 12:27:27 +0100

    folders renamed

---
+++ poker-client/src/test/java/pl/edu/agh/kis/pz1/ClientTest.java
@@ -0,0 +1,22 @@
+package pl.edu.agh.kis.pz1;
+
+import static junit.framework.TestCase.assertNotNull;
+import org.junit.Test;
+
+
+public class ClientTest {
+
+
+    /**
+     * Test for the construction of Main and the 
+     * main method being called
+     *
+     */
+    @Test
+    public void shouldCreateMainObject(){
+        Client main = new Client();
+        assertNotNull("Main method called on class Main2.", main);
+    }
+}
+
+
